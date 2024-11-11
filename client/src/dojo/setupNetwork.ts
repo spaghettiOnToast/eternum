9ba6dbda583ea659668a87fdbde0a400dc3c56c4
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-11 17:11:34 +1100

    rever

--- client/src/dojo/setupNetwork.ts
+++ client/src/dojo/setupNetwork.ts
@@ -26,16 +26,16 @@ export async function setupNetwork({ ...config }: DojoConfig) {
     feeTokenAddress: config.feeTokenAddress,
   });
 
-  // if (import.meta.env.VITE_PUBLIC_DEV === "true") {
   try {
     await burnerManager.init();
-    if (burnerManager.list().length === 0) {
-      await burnerManager.create();
+    if (import.meta.env.VITE_PUBLIC_DEV === "true") {
+      if (burnerManager.list().length === 0) {
+        await burnerManager.create();
+      }
     }
   } catch (e) {
     console.error(e);
   }
-  // }
 
   return {
     toriiClient,
